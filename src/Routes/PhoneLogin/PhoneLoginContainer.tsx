4785700f436faa303b0f96e31e26e9010970df4a
Uco <mmdsds@u.sogang.ac.kr>
2019-01-25 04:14:25 +0800

    Mutation & codegen settings

--- src/Routes/PhoneLogin/PhoneLoginContainer.tsx
+++ src/Routes/PhoneLogin/PhoneLoginContainer.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { Mutation } from 'react-apollo';
 import { RouteComponentProps } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import PhoneLoginPresenter from "./PhoneLoginPresenter";
+import { PHONE_SIGN_IN } from './PhoneQueries.queries';
 
 interface IState {
   countryCode: string;
   phoneNumber: string;
 }
+
+interface IMutationInterface {
+  phoneNumber: string;
+}
+
+class PhoneSignInMutation extends Mutation<any, IMutationInterface>{}
 
 class PhoneLoginContainer extends React.Component<
   RouteComponentProps<any>,
@@ -18,28 +27,45 @@ class PhoneLoginContainer extends React.Component<
 
   public render() {
     const { countryCode, phoneNumber } = this.state;
-    return( 
-      <PhoneLoginPresenter
-        countryCode={countryCode}
-        phoneNumber={phoneNumber}
-        onInputChange={this.onInputChange}
-        onSubmit={this.onSubmit}
-    />);
+    return (
+      <PhoneSignInMutation
+        mutation={PHONE_SIGN_IN}
+        variables={{
+          phoneNumber: `${countryCode}${phoneNumber}`
+        }}
+      >
+        {(mutation, { loading }) => {
+          const onSubmit: React.FormEventHandler<HTMLFormElement> = event => {
+            event.preventDefault();
+            const isValid = /^\+[1-9]{1}[0-9]{7,11}$/.test(
+              `${countryCode}${phoneNumber}`
+            );
+            if (isValid) {
+              mutation();
+            } else {
+              toast.error("Please write a valid phone number");
+            }
+          };
+          return (
+            <PhoneLoginPresenter
+              countryCode={countryCode}
+              phoneNumber={phoneNumber}
+              onInputChange={this.onInputChange}
+              onSubmit={onSubmit}
+            />
+          );
+        }}
+      </PhoneSignInMutation>
+    
+    );
   }
 
   public onInputChange: React.ChangeEventHandler<HTMLInputElement | HTMLSelectElement> = (event) => {
-    const { target: {name, value} } = event;
+    const { target: { name, value } } = event;
     this.setState({
       [name]: value
     } as any);
   };
-
-  public onSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
-    event.preventDefault();
-    const { countryCode, phoneNumber} = this.state;
-    // tslint:disable-next-line
-    console.log(countryCode, phoneNumber);
-  }
-};
+}
 
 export default PhoneLoginContainer;
